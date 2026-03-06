15cf1079d18b4532877edbd8646beb40c64d9570
Fredz <kaberiafredric@gmail.com>
2026-03-06 15:47:55 +0300

    email routing

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   //   reactStrictMode: true,
   // swcMinify: true,
+   async redirects() {
+    return [
+      {
+        source: '/webmail',
+        destination: 'https://business130.web-hosting.com:2096/webmaillogout.cgi', 
+        permanent: false, 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
+
